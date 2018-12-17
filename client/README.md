190a4381785c731084cca0c48496dbaf9a8c03da
James Gary <gmarchesi888@gmail.com>
2018-12-17 16:55:07 -0500

    Added some functionality to App.js

--- client/README.md
+++
@@ -1,2 +0,0 @@
-Based on tutorial at:
-https://medium.com/javascript-in-plain-english/full-stack-mongodb-react-node-js-express-js-in-one-simple-app-6cc8ed6de274
--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,47 @@ import axios from "axios";
 import "./App.css";
 
 class App extends Component {
+  //initialize state
+  state = {
+    data: [],
+    id: 0,
+    message: null,
+    intervalIsSet: false,
+    idToDelete: null,
+    idToUpdate: null,
+    objectToUpdate: null
+  };
+
+  // fetch all data in DB - poll DB for changes
+  componentDidMount() {
+    this.getDataFromDb();
+    if (!this.state.intervalIsSet) {
+      let interval = setInterval(this.getDataFromDb, 1000);
+      this.setState({ intervalIsSet: interval });
+    }
+  }
+
+  // stop checking the DB for changes when the component unmounts
+  // ** is this necessary? the component doesn't exist after it unmounts **
+  componentWillUnmount() {
+    if (this.state.intervalIsSet) {
+      clearInterval(this.state.intervalIsSet);
+      this.state({ intervalIsSet: null });
+    }
+  }
+
+  putDataToDB = message => {
+    let currentIds = this.state.data.map(data => data.id);
+    let idToBeAdded = 0;
+    while (currentIds.includes(idToBeAdded)) {
+      idToBeAdded += 1;
+    }
+    axios.post("/api/putData", {
+      id: idToBeAdded,
+      message: message,
+    });
+  };
+
   render() {
     return <div>JIM IS STILL HERE</div>;
   }
